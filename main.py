41d2de15e3c84d8187469bdb35b8337661f52102
Anthony Parrett <anthonyparrett7@gmail.com>
2023-11-20 20:04:40 -0600

    ability to process directories

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ import datetime
 import math
 import os
 import ntpath
+import glob
 
 ### SETTINGS ###
 
-filePath = "medium.mp4"
+# Example file: C:\\Users\\username\\Documents\\files\\video.mp4
+# Example folder: C:\\Users\\username\\Documents\\files 
+filePath = "C:\\Users\\aparr\\Documents\\Stars\\twinkle.mp4"
 
 # if the object in motion is moving slower than the speedThreshold, it will be ignored *after the initial detection*.
 # default: 15
@@ -31,107 +34,115 @@ debounceLimit = 3
 
 # change to True to show a video with rectangles around detected motion
 # this can be useful for playing with the thresholds
-showVideo = False 
+showVideo = True 
 
 # change to True to include an outline (a green rectangle) of the detected motion in the saved screenshot
 saveOutline = True
 
-
 ### Program ###
 
+print('Starting meteor detection.')
+
+def processVideo(filePath):
+  print(f'Processing video at {filePath}')
+  fileName = ntpath.basename(filePath)
+
+  outputPath = 'logs/' + os.path.splitext(fileName)[0]
+  outputFolderExists = os.path.exists(outputPath)
+  if outputFolderExists == False:
+    os.makedirs(outputPath)
+
+  outputLogPath = outputPath + '/log.txt'
+  # outputFileExists = os.path.isfile(outputLogPath)
+  # if outputFileExists == False:
+  log = open(outputLogPath, "a")
+
+  video = cv2.VideoCapture(filePath)
+  fps = video.get(cv2.CAP_PROP_FPS)
+  background = None
+  i = 0
+  debounceCount = 0
+  prevX = 0
+  prevY = 0
+
+  while True:
+    i += 1
+    status, frame = video.read()
+
+    if status == False:
+      break
+
+    # i % 5 changes the background throughout the video so that stars aren't detected
+    if i % 5 == 1:
+      background = frame
+      background = cv2.cvtColor(background,cv2.COLOR_BGR2GRAY)
+      background = cv2.GaussianBlur(background,(blurThreshold,blurThreshold),0)
+      if i == 1:
+        continue
+
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray,(blurThreshold,blurThreshold), 0)
+
+    diff = cv2.absdiff(background,gray)
+
+    thresh = cv2.threshold(diff,differenceThreshold,255,cv2.THRESH_BINARY)[1]
+    thresh = cv2.dilate(thresh, None, iterations = 2)
+
+    cnts,res = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    j = 0
+    for contour in cnts:
+      if cv2.contourArea(contour) > sizeOfMovementThreshold :
+        j += 1
+
+        (x,y,w,h) = cv2.boundingRect(contour)
+
+        distanceMoved = abs(prevX - x) + abs(prevY - y)
+        if distanceMoved < motionSpeedThreshold:
+          prevX = x
+          prevY = y
+          continue
+
+        if j == 1: # only log a single detected motion per frame
+          if debounceCount == 0:
+            seconds = i / fps
+            secondsDisplay = math.floor((seconds % 60 * 10)) / 10
+            minutesDisplay = math.floor((seconds / 60) % 60)
+            hoursDisplay = math.floor(seconds / 60 / 60)
+            motionTimestamp = f'{hoursDisplay}h-{minutesDisplay}m-{secondsDisplay}s'
+            log.write(f'Detected motion at {motionTimestamp} \n')
+            print(f'Detected motion at {motionTimestamp}')
+
+            if (saveOutline == True):
+              cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0), 1)
+            cv2.imwrite(f'{outputPath}/motion-{motionTimestamp}.jpg', frame)
+
+          debounceCount += 1
+          if debounceCount == debounceLimit:
+            debounceCount = 0
+
+        prevX = x
+        prevY = y
+        if (showVideo == True):
+          cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0), 1)
+
+    if (showVideo == True):
+      cv2.imshow("All Contours", frame)
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+      break
+
+  log.close()
+  video.release()
+
 # Don't touch anything below this
-if os.path.exists(filePath) == False:
-	print(f'Could not find file at {filePath}. Exiting.')
-	exit()
+if os.path.isdir(filePath):
+  for file in glob.iglob(f'{filePath}\\*', recursive=True):
+    processVideo(file)
+elif os.path.isfile(filePath):
+  processVideo(filePath)
+else:
+  print(f'Could not find file at {filePath}. Exiting.')
 
-fileName = ntpath.basename(filePath)
-
-outputPath = 'logs/' + os.path.splitext(fileName)[0]
-outputFolderExists = os.path.exists(outputPath)
-if outputFolderExists == False:
-	os.makedirs(outputPath)
-
-outputLogPath = outputPath + '/log.txt'
-# outputFileExists = os.path.isfile(outputLogPath)
-# if outputFileExists == False:
-log = open(outputLogPath, "a")
-
-
-video = cv2.VideoCapture(filePath)
-fps = video.get(cv2.CAP_PROP_FPS)
-background = None
-i = 0
-debounceCount = 0
-prevX = 0
-prevY = 0
-
-while True:
-	i += 1
-	status, frame = video.read()
-
-	if status == False:
-		break
-
-	# i % 5 changes the background throughout the video so that stars aren't detected
-	if i % 5 == 1:
-		background = frame
-		background = cv2.cvtColor(background,cv2.COLOR_BGR2GRAY)
-		background = cv2.GaussianBlur(background,(blurThreshold,blurThreshold),0)
-		if i == 1:
-			continue
-
-	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-	gray = cv2.GaussianBlur(gray,(blurThreshold,blurThreshold), 0)
-
-	diff = cv2.absdiff(background,gray)
-
-	thresh = cv2.threshold(diff,differenceThreshold,255,cv2.THRESH_BINARY)[1]
-	thresh = cv2.dilate(thresh, None, iterations = 2)
-
-	cnts,res = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-	j = 0
-	for contour in cnts:
-		if cv2.contourArea(contour) > sizeOfMovementThreshold :
-			j += 1
-
-			(x,y,w,h) = cv2.boundingRect(contour)
-
-			distanceMoved = abs(prevX - x) + abs(prevY - y)
-			if distanceMoved < motionSpeedThreshold:
-				prevX = x
-				prevY = y
-				continue
-
-			if j == 1: # only log a single detected motion per frame
-				if debounceCount == 0:
-					seconds = i / fps
-					secondsDisplay = math.floor((seconds % 60 * 10)) / 10
-					minutesDisplay = math.floor((seconds / 60) % 60)
-					hoursDisplay = math.floor(seconds / 60 / 60)
-					motionTimestamp = f'{hoursDisplay}h-{minutesDisplay}m-{secondsDisplay}s'
-					log.write(f'Detected motion at {motionTimestamp} \n')
-					print(f'Detected motion at {motionTimestamp}')
-
-					if (saveOutline == True):
-						cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0), 1)
-					cv2.imwrite(f'{outputPath}/motion-{motionTimestamp}.jpg', frame)
-
-				debounceCount += 1
-				if debounceCount == debounceLimit:
-					debounceCount = 0
-
-			prevX = x
-			prevY = y
-			if (showVideo == True):
-				cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0), 1)
-
-	if (showVideo == True):
-		cv2.imshow("All Contours", frame)
-
-	key = cv2.waitKey(1)
-	if key == ord('q'):
-		break
-
-log.close()
-video.release()
+print('Finished processing.')
